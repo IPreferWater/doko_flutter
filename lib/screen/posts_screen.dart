97e4a7c6eca31365a1ab03ea91ecdcafb89f3205
IPreferWater <ipreferwater2@gmail.com>
2021-04-02 10:17:39 +0200

    feat(init): add locationWidget in postFormDialog

--- lib/screen/posts_screen.dart
+++ lib/screen/posts_screen.dart
@@ -62,7 +62,6 @@ class _PostsScreenState extends State<PostsScreen> {
     return
       Column(
           children: <Widget>[
-          _gpsWidget(),
     FloatingActionButton(
     child: Icon(Icons.send),
     heroTag: "sendPostBtn",
@@ -77,74 +76,9 @@ class _PostsScreenState extends State<PostsScreen> {
     }
   }
 
-  Widget _gpsWidget() {
-    TextField latitudeTextField = TextField(
-      controller: new TextEditingController(text: ""),
-      decoration: new InputDecoration(labelText: "latitude"),
-      keyboardType: TextInputType.number,
-    );
 
-    TextField longitudeTextField = TextField(
-      controller: new TextEditingController(text: ""),
-      decoration: new InputDecoration(labelText: "longitude"),
-      keyboardType: TextInputType.number,
-    );
-    return Column(
-        children: <Widget>[
-          latitudeTextField,
-          longitudeTextField,
-          FloatingActionButton(
-              child: Icon(Icons.gps_fixed),
-              heroTag: "getLocationBtn",
-              onPressed: () async {
-                Position p = await _determinePosition();
-                latitudeTextField.controller.text = p.latitude.toString();
-                longitudeTextField.controller.text = p.longitude.toString();
-              })
-        ]);
-  }
 
-  /// Determine the current position of the device.
-  ///
-  /// When the location services are not enabled or permissions
-  /// are denied the `Future` will return an error.
-  Future<Position> _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
 
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.deniedForever) {
-        // Permissions are denied forever, handle appropriately.
-        return Future.error(
-            'Location permissions are permanently denied, we cannot request permissions.');
-      }
-
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
-        return Future.error(
-            'Location permissions are denied');
-      }
-    }
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    return await Geolocator.getCurrentPosition();
-  }
 
 //TODO: client should be injected
   Future _testGraphQl(GraphQLClient client) async {
--- lib/widget/post_form_dialog.dart
+++ lib/widget/post_form_dialog.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_doko/model/post.dart';
+import 'package:geolocator/geolocator.dart';
 
 class PostFormDialog extends StatefulWidget{
 
@@ -18,6 +19,8 @@ class _PostFormDialogState extends State<PostFormDialog> {
   final _formKey = GlobalKey<FormState>();
   TextFormField titleFormField;
   TextFormField textFormField;
+  TextField latitudeTextField;
+  TextField longitudeTextField;
 
 
   @override
@@ -26,6 +29,8 @@ class _PostFormDialogState extends State<PostFormDialog> {
 
     titleFormField = createTextFormField("title");
     textFormField = createTextFormField("text");
+    latitudeTextField = createLocationTextField("latitude");
+    longitudeTextField = createLocationTextField("longitude");
 
     if (widget.post != null){
       //init for update
@@ -48,7 +53,7 @@ class _PostFormDialogState extends State<PostFormDialog> {
         key: _formKey,
         child: Column(
             children: <Widget>[
-              //_gpsWidget(),
+              _locationWidget(),
               titleFormField,
               textFormField,
               FloatingActionButton(
@@ -56,19 +61,77 @@ class _PostFormDialogState extends State<PostFormDialog> {
                   child: Icon(Icons.add),
                   heroTag: "addOrUpdatePostBtn",
                   onPressed: ()  {
-
                     Post post = new Post(
                         title: titleFormField.controller.text,
                         text: textFormField.controller.text,
-                        latitude: 5.45678,
-                        longitude: 7.56789
+                        latitude: double.parse(latitudeTextField.controller.text),
+                        longitude: double.parse(longitudeTextField.controller.text),
                     );
-
                     Navigator.pop(context, post);
                   })
             ]
         )
     );
+  }
+
+
+
+  Widget _locationWidget() {
+    return Column(
+        children: <Widget>[
+          latitudeTextField,
+          longitudeTextField,
+          FloatingActionButton(
+              child: Icon(Icons.gps_fixed),
+              heroTag: "getLocationBtn",
+              onPressed: () async {
+                Position p = await _determinePosition();
+                latitudeTextField.controller.text = p.latitude.toString();
+                longitudeTextField.controller.text = p.longitude.toString();
+              })
+        ]);
+  }
+
+  /// Determine the current position of the device.
+  ///
+  /// When the location services are not enabled or permissions
+  /// are denied the `Future` will return an error.
+  Future<Position> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.deniedForever) {
+        // Permissions are denied forever, handle appropriately.
+        return Future.error(
+            'Location permissions are permanently denied, we cannot request permissions.');
+      }
+
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error(
+            'Location permissions are denied');
+      }
+    }
+
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    return await Geolocator.getCurrentPosition();
   }
 
   TextFormField createTextFormField(String str ){
@@ -84,4 +147,13 @@ class _PostFormDialogState extends State<PostFormDialog> {
         }
     );
   }
+
+  createLocationTextField(String str ){
+    return TextField(
+      controller: new TextEditingController(text: ""),
+      decoration: new InputDecoration(labelText: str),
+      keyboardType: TextInputType.number,
+    );
+  }
+
 }
